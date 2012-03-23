2124b2b1f56f55b260e8a6e00a5d7458780a7540
regtan <regtan1210@gmail.com>
2012-03-23 19:02:21 +0900

    mock化できてなかったので修正

--- src/test/java/org/kirino/coderwall4j/scrape/CoderwallTeamHtmlScraperTest.java
+++ src/test/java/org/kirino/coderwall4j/scrape/CoderwallTeamHtmlScraperTest.java
@@ -3,18 +3,30 @@ package org.kirino.coderwall4j.scrape;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import static org.junit.matchers.JUnitMatchers.hasItems;
-import mockit.Mockit;
 
+import java.io.File;
+
+import mockit.Expectations;
+import mockit.Mocked;
+
+import org.apache.commons.io.FileUtils;
+import org.jsoup.Jsoup;
 import org.junit.Test;
 import org.kirino.coderwall4j.model.Team;
 import org.kirino.coderwall4j.util.HttpClient;
 
 public class CoderwallTeamHtmlScraperTest {
-
+	@Mocked
+	final HttpClient httpClient = null;
 
 	@Test
 	public void nicobookのデータが取れること() throws Exception {
-		Mockit.setUpMock(HttpClient.class, HttpClientMock.class);
+		new Expectations() {
+			{
+				HttpClient.getDocument("4f61c42bdb6418000a000001");
+				result = Jsoup.parse(FileUtils.readFileToString(new File("src/test/resources/nicobook.html")));
+			}
+		};
 
 		CoderwallTeamHtmlScraper cts = new CoderwallTeamHtmlScraper("4f61c42bdb6418000a000001");
 		Team team = cts.scrapeTeamData();
@@ -22,7 +34,8 @@ public class CoderwallTeamHtmlScraperTest {
 		assertThat(team.getUrl(), is("http://seiga.nicovideo.jp/book/"));
 		assertThat(team.getLocation(), is("Hamacho"));
 		assertThat(team.getAbout(), is("The Team Nicobook in Dwango inc."));
-		assertThat(team.getCoreSkills(), hasItems("ruby", "javascript", "java", "coffeescript", "c", "mysql", "git", "objective-c"));
+		assertThat(team.getCoreSkills(),
+				hasItems("ruby", "javascript", "java", "coffeescript", "c", "mysql", "git", "objective-c"));
 
 		assertThat(team.getTeamAchievements().get(0).getAchievementNo(), is(7L));
 		assertThat(team.getTeamAchievements().get(0).getAchievementName(), is("Charity"));
@@ -36,7 +49,7 @@ public class CoderwallTeamHtmlScraperTest {
 		assertThat(team.getMembers().get(0).getUserName(), is("yamada masaki"));
 		assertThat(team.getMembers().get(0).getDiscription(), is("Anime Driven Development"));
 		assertThat(team.getMembers().get(0).getAchievements(), is(9L));
-		assertThat(team.getMembers().get(0).getEndorsements(), is(1L));
+		assertThat(team.getMembers().get(0).getEndorsements(), is(0L));
 	}
 
 }
